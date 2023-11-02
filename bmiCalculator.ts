35c53a7bac5eee33caab83bba8c73214ffac1d3a
ad.hanif <ad.hanif@outlook.com>
2023-11-02 12:14:46 +0100

    9.5 WebBMI completed

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,18 +1,37 @@
-const calculateBmi = (height: number, weight: number) => {
-  let bmi: number = parseFloat((weight / Math.pow(height / 100, 2)).toFixed(2));
+interface values {
+  heightCM: number;
+  weightKG: number;
+}
 
-  if (bmi < 18.5) {
-    console.log("Not Normal (Underweight)");
-  } else if (bmi >= 18.5 && bmi <= 24.9) {
-    console.log("Normal (healthy weight)");
-  } else if (bmi > 25 && bmi <= 29.9) {
-    console.log("Not Normal (Overweight)");
-  } else if (bmi >= 30) {
-    console.log("Not Normal (Obesity)");
+export const parsedBmiValues = (height: number, weight: number): values => {
+  if (!isNaN(height) && !isNaN(weight)) {
+    return {
+      heightCM: height,
+      weightKG: weight,
+    };
+  } else {
+    throw new Error("Invalid input types");
   }
 };
 
-const height: number = Number(process.argv[2]);
-const weight: number = Number(process.argv[3]);
+export const bmiCalculate = (height: number, weight: number): string => {
+  let bmi: number = parseFloat((weight / Math.pow(height / 100, 2)).toFixed(2));
 
-calculateBmi(height, weight);
+  if (bmi < 18.5) {
+    return "Not Normal (Underweight)";
+  } else if (bmi >= 18.5 && bmi <= 24.9) {
+    return "Normal (healthy weight)";
+  } else if (bmi > 25 && bmi <= 29.9) {
+    return "Not Normal (Overweight)";
+  } else if (bmi >= 30) {
+    return "Not Normal (Obesity)";
+  }
+  // Default return statement
+  return "Unknown BMI Category";
+};
+
+// export default calculateBmi;
+// const height: number = Number(process.argv[2]);
+// const weight: number = Number(process.argv[3]);
+
+// calculateBmi(height, weight);
