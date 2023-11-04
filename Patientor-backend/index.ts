ce32777825ac5a55f4343c89b58b29b42e2b51ff
ad.hanif <ad.hanif@outlook.com>
2023-11-04 11:34:53 +0100

    9.9: Patientor backend, step2 completed

--- Patientor-backend/index.ts
+++ Patientor-backend/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
 const app = express();
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
+
+app.get("/api/patients", (_req, res) => {
+  console.log("someone pinged here");
+  res.send("pong");
+});
 
 app.get("/api/ping", (_req, res) => {
   console.log("someone pinged here");
