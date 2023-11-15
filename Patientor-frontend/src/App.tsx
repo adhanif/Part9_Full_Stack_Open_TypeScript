fb86704f793a78cddd351d62e323867bb6b4cc34
ad.hanif <ad.hanif@outlook.com>
2023-11-15 16:34:20 +0100

    Exercise 9.15 completed

--- Patientor-frontend/src/App.tsx
+++ Patientor-frontend/src/App.tsx
@@ -3,20 +3,50 @@ import Header from "../src/components/Header";
 import Content from "./components/Content";
 import TotalExercises from "./components/Total";
 
+import { CoursePart } from "./types";
+
 function App() {
   const courseName: string = "Half Stack application development";
-  const courseParts = [
+
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is an awesome course part",
+      kind: "basic",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      kind: "group",
+    },
+    {
+      name: "Basics of type Narrowing",
+      exerciseCount: 7,
+      description: "How to go from unknown to string",
+      kind: "basic",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      backgroundMaterial:
+        "https://type-level-typescript.com/template-literal-types",
+      kind: "background",
+    },
+    {
+      name: "TypeScript in frontend",
+      exerciseCount: 10,
+      description: "a hard part",
+      kind: "basic",
+    },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      kind: "special",
     },
   ];
 
