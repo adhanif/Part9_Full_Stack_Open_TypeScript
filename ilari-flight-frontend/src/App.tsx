cfe750c7f357c3bd7b20c7c846380caa3f70458b
ad.hanif <ad.hanif@outlook.com>
2023-11-16 15:22:16 +0100

    Exercise 9.17 completed

--- ilari-flight-frontend/src/App.tsx
+++ ilari-flight-frontend/src/App.tsx
@@ -7,6 +7,10 @@ import Entries from "../src/components/Entries";
 
 function App() {
   const [entries, setEntries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState("");
+  const [visibility, setVisibility] = useState("");
+  const [weather, setWeather] = useState("");
+  const [comment, setComment] = useState("");
 
   useEffect(() => {
     axios.get("http://localhost:3000/api/diaries").then((res) => {
@@ -14,10 +18,44 @@ function App() {
     });
   }, []);
 
-  console.log(entries);
+  const entryCreation = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+    const entryToAdd = {
+      date: date,
+      visibility: visibility,
+      weather: weather,
+      comment: comment,
+      id: entries.length + 1,
+    };
+    setEntries(entries.concat(entryToAdd));
+    console.log(entryToAdd);
+  };
 
   return (
     <>
+      <h1> add new Entry</h1>
+      <form onSubmit={entryCreation}>
+        <div>
+          date <input value={date} onChange={(e) => setDate(e.target.value)} />
+        </div>
+        <div>
+          visibility{" "}
+          <input
+            value={visibility}
+            onChange={(e) => setVisibility(e.target.value)}
+          />
+        </div>
+        <div>
+          weather{" "}
+          <input value={weather} onChange={(e) => setWeather(e.target.value)} />
+        </div>
+        <div>
+          comment{" "}
+          <input value={comment} onChange={(e) => setComment(e.target.value)} />
+        </div>
+
+        <button type="submit">add</button>
+      </form>
       <h1>Diary entries</h1>
       <Entries entries={entries} />
     </>
