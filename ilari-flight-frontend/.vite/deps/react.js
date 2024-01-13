651ff7ac067dfce59b0067df33f820c2657893c0
ad.hanif <ad.hanif@outlook.com>
2024-01-13 14:02:00 +0200

    New begining

--- ilari-flight-frontend/.vite/deps/react.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react
-} from "./chunk-IMHGSZHV.js";
-import "./chunk-6TJCVOLN.js";
-export default require_react();
-//# sourceMappingURL=react.js.map
--- ilari-flight-frontend/src/components/RadioButton.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-type Props = {};
-
-const RadioButton = (props: Props) => {
-  return <div>RadioButton</div>;
-};
-
-export default RadioButton;
