8d55ea4afffe0d31b1c9be6c4a0efa7b8177be46
ad.hanif <ad.hanif@outlook.com>
2024-01-15 13:20:16 +0200

    9.6 Eslint completed

--- fitst-steps-typescript/exerciseCalculator.ts
+++ fitst-steps-typescript/exerciseCalculator.ts
@@ -1,33 +1,33 @@
 interface Exercise {
-  periodLength: number
-  trainingDays: number
-  success: boolean
-  rating: number
-  ratingDescription: string
-  target: number
-  average: number
+  periodLength: number;
+  trainingDays: number;
+  success: boolean;
+  rating: number;
+  ratingDescription: string;
+  target: number;
+  average: number;
 }
 
 interface MultiplyValues {
-  values: number[]
+  values: number[];
 }
 
-const isNotNumber = (arg: any): Boolean => isNaN(Number(arg))
+const isNotNumber = (arg: number): boolean => isNaN(Number(arg));
 
 const parseArguments = (args: string[]): MultiplyValues => {
-  const values = args.slice(3).map(Number)
+  const values = args.slice(3).map(Number);
   if (values.some(isNotNumber)) {
-    throw new Error('Provided values were not correct!')
+    throw new Error('Provided values were not correct!');
   }
-  return { values }
-}
+  return { values };
+};
 
 const calculateExercises = (target: number, values: number[]): Exercise => {
-  const trainingDays: number = values.filter((ele) => ele !== 0).length
+  const trainingDays: number = values.filter((ele) => ele !== 0).length;
   const averageHours: number =
     values.reduce((tot, curr) => {
-      return tot + curr
-    }, 0) / values.length
+      return tot + curr;
+    }, 0) / values.length;
 
   return {
     periodLength: values.length,
@@ -40,17 +40,21 @@ const calculateExercises = (target: number, values: number[]): Exercise => {
         : 'not too bad but could be better',
     target: target,
     average: averageHours,
-  }
-}
+  };
+};
 
 try {
-  const args = process.argv
-  const targetValue = Number(args[2])
+  const args = process.argv;
+  const targetValue = Number(args[2]);
 
-  const parsedValues = parseArguments(args)
-  const exerciseResult = calculateExercises(targetValue, parsedValues.values)
+  const parsedValues = parseArguments(args);
+  const exerciseResult = calculateExercises(targetValue, parsedValues.values);
 
-  console.log(exerciseResult)
-} catch (e) {
-  console.error(e.message)
+  console.log(exerciseResult);
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
 }
