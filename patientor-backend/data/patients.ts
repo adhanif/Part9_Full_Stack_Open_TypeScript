e93181fd357f083e2f090e00d727620c5f77cc46
ad.hanif <ad.hanif@outlook.com>
2024-01-18 13:23:03 +0200

    9.11: Patientor backend, step4

--- patientor-backend/data/patients.ts
+++ patientor-backend/data/patients.ts
@@ -1,4 +1,6 @@
-const data = [
+import { PatientEntry } from '../src/types/patientsTypes';
+
+const patientsEntries: PatientEntry[] = [
   {
     id: 'd2773336-f723-11e9-8f0b-362b9e155667',
     name: 'John McClane',
@@ -41,4 +43,4 @@ const data = [
   },
 ];
 
-export default data;
+export default patientsEntries;
--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import diagnosesRouter from './src/routes/diagnoses';
+import patientsRouter from './src/routes/patients';
 
 const app = express();
 app.use(express.json());
@@ -13,6 +14,8 @@ app.get('/api/ping', (_req, res) => {
 });
 
 app.use('/api/diagnoses', diagnosesRouter);
+
+app.use('/api/patients', patientsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port http://localhost:${PORT}`);
