46cef6da6f4993a4a0cea9b6db861805330b7fa1
ad.hanif <ad.hanif@outlook.com>
2024-01-19 11:49:31 +0200

    9.13: Patientor backend, step6 completed

--- patientor-backend/data/patients.ts
+++ patientor-backend/data/patients.ts
@@ -1,6 +1,8 @@
 import { PatientEntry } from '../src/types/patientsTypes';
+import toNewPatientEntry from '../src/utils/utils';
 
-const patientsEntries: PatientEntry[] = [
+// const patientsEntries: PatientEntry[] = [
+const data = [
   {
     id: 'd2773336-f723-11e9-8f0b-362b9e155667',
     name: 'John McClane',
@@ -42,5 +44,11 @@ const patientsEntries: PatientEntry[] = [
     occupation: 'Digital evangelist',
   },
 ];
+
+const patientsEntries: PatientEntry[] = data.map((obj) => {
+  const object = toNewPatientEntry(obj) as PatientEntry;
+  object.id = obj.id;
+  return object;
+});
 
 export default patientsEntries;
