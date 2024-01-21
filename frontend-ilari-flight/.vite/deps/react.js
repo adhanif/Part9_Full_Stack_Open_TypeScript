8d2f1004276fa57aeb172126f665befe9cdd91bf
ad.hanif <ad.hanif@outlook.com>
2024-01-21 14:47:49 +0200

    Exercise 9.17 completed

---
+++ frontend-ilari-flight/.vite/deps/react.js
@@ -0,0 +1,6 @@
+import {
+  require_react
+} from "./chunk-IMHGSZHV.js";
+import "./chunk-6TJCVOLN.js";
+export default require_react();
+//# sourceMappingURL=react.js.map
--- frontend-ilari-flight/src/App.tsx
+++ frontend-ilari-flight/src/App.tsx
@@ -1,26 +1,85 @@
+import React from 'react';
 import { useState, useEffect } from 'react';
-import { getAllDiaries } from './diaryService';
+import { getAllDiaries, createNewDiary } from './diaryService';
 import { DiaryEntry } from '../src/types';
 import './App.css';
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+  const [date, setDate] = useState('');
+  const [visibility, setVisibilty] = useState('');
+  const [weather, setweather] = useState('');
+  const [comment, setComment] = useState('');
 
   useEffect(() => {
     getAllDiaries().then((data) => setDiaries(data));
   }, []);
 
+  const diaryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const diaryToAdd: DiaryEntry = {
+      id: diaries.length + 1,
+      date: date,
+      weather: weather,
+      visibility: visibility,
+      comment: comment,
+    };
+
+    createNewDiary(diaryToAdd).then((data) => setDiaries(diaries.concat(data)));
+    setDiaries(diaries.concat(diaryToAdd));
+    setDate('');
+    setVisibilty('');
+    setweather('');
+    setComment('');
+  };
+
   return (
     <>
-      <h1>Diary Entries</h1>
-      {diaries.map((diary) => (
-        <div key={diary.id}>
-          <h3>{diary.date}</h3>
-          <p>visibility: {diary.visibility}</p>
-          <p>weather: {diary.weather}</p>
-          <p>comment: {diary.comment}</p>
-        </div>
-      ))}
+      <div>
+        <form onSubmit={diaryCreation}>
+          <div>
+            date:
+            <input
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
+            />
+          </div>
+          <div>
+            visibilty:
+            <input
+              value={visibility}
+              onChange={(event) => setVisibilty(event.target.value)}
+            />
+          </div>
+          <div>
+            weather:
+            <input
+              value={weather}
+              onChange={(event) => setweather(event.target.value)}
+            />
+          </div>
+          <div>
+            comment:
+            <input
+              value={comment}
+              onChange={(event) => setComment(event.target.value)}
+            />
+          </div>
+          <button type='submit'>add</button>
+        </form>
+      </div>
+
+      <div>
+        <h1>Diary Entries</h1>
+        {diaries.map((diary) => (
+          <div key={diary.id}>
+            <h3>{diary.date}</h3>
+            <p>visibility: {diary.visibility}</p>
+            <p>weather: {diary.weather}</p>
+            <p>comment: {diary.comment}</p>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
